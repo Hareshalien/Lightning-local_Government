50cd1b2e37a49c493b90f90536c3cf6c5bd727f9
Hareshalien <135260313+Hareshalien@users.noreply.github.com>
2026-01-29 20:09:23 +0800

    Update Firebase configuration with actual values

--- constants.ts
+++ constants.ts
@@ -1,12 +1,12 @@
 // Configuration provided by the user
 export const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "" // Inferred standard format
+  apiKey: "AIzaSyDcbs_h9FlmT02EEyOUez4bf0nOb3vcQG8",
+  authDomain: "bios-2d8ca.firebaseapp.com",
+  databaseURL: "https://bios-2d8ca-default-rtdb.firebaseio.com",
+  projectId: "bios-2d8ca",
+  storageBucket: "bios-2d8ca.firebasestorage.app",
+  messagingSenderId: "40622594695",
+  appId: "1:40622594695:web:unknown" // Inferred standard format
 };
 
 export const GOOGLE_MAPS_API_KEY = "";
